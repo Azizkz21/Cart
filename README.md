2b32e71605844806f3191c4d36886b68c60d7fbf
Aziz Maxsudov <mikasa1353@gmail.com>
2025-11-09 19:18:26 +0500

    initial project version

---
+++ README.md
@@ -0,0 +1,25 @@
+# React Shopping Cart App
+
+This is a simple React + TypeScript  project built with Vite that demonstrates how to create a small e-commerce store with a shopping cart.  
+It uses React Context API for global state management and React Bootstrap for layout and styling.
+
+
+##  Features
+
+-  Multi-page navigation (Home, Store, About) using - React Router
+-  Product listing with data loaded from a JSON file
+-  Add, remove, and update items in the shopping cart
+-  Automatic total price calculation
+-  Responsive navigation bar with a dynamic cart icon
+-  Fast and modern setup using - Vite
+
+
+- React + TypeScript
+- Vite
+- React Router
+- React Bootstrap
+- Context API
+- ESLint + Prettier (optional for code formatting)
+
+
+
---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,106 @@
+import { createContext, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+interface ShoppingCartProviderProps {
+  children?: React.ReactNode;
+}
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+interface ShoppingCartContext {
+  openCart: () => void;
+  closeCart: () => void;
+  getItemQuantity: (id: number) => number;
+  increaseCardQuantity: (id: number) => void;
+  decreaseCardQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    "shopping-cart",
+    []
+  );
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  function increaseCardQuantity(id: number) {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id) == null) {
+        return [...currentItems, { id, quantity: 1 }];
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function decreaseCardQuantity(id: number) {
+    setCartItems((currentItems) => {
+      const foundItem = currentItems.find((item) => item.id === id);
+      if (foundItem && foundItem.quantity === 1) {
+        return currentItems.filter((item) => item.id !== id);
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems((currentItems) => {
+      return currentItems.filter((item) => item.id !== id);
+    });
+  }
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCardQuantity,
+        decreaseCardQuantity,
+        removeFromCart,
+        openCart,
+        closeCart,
+        cartItems,
+        cartQuantity,
+      }}
+    >
+      {children}
+      <ShoppingCart isOpen={isOpen} />
+    </ShoppingCartContext.Provider>
+  );
+}
